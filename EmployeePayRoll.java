b4862eaa394cad7bb3ec71e93677e554e36c498a
thesrt <122377381+thesrt@users.noreply.github.com>
2023-01-25 20:10:08 +0530

    Add files via upload

---
+++ EmployeePayRoll.java
@@ -0,0 +1,27 @@
+package CipherSchoolCourse;
+
+import java.util.Scanner;
+
+public class EmployeePayRoll {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter BasicPay");
+			
+			double basicpay = sc.nextInt();
+			double hra=0,da=0;
+			if(basicpay >= 5000)
+			{
+				hra= 0.1 * basicpay;
+				da= 0.2 * basicpay;
+			}
+			else
+			{
+				hra= 0.2* basicpay;
+				da= 0.3 * basicpay;
+			}
+			System.out.println(hra+" "+da);
+			
+			System.out.println(basicpay+ hra+ da);
+		}
+	}
+}
